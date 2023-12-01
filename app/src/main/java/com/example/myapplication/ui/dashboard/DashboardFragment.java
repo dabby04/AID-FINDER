27978ea004a78c3195c707ceefd8e538e182f338
dabby04 <97193496+dabby04@users.noreply.github.com>
2023-11-30 17:11:16 -0800

    getting location done

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -1,20 +1,54 @@
 package com.example.myapplication.ui.dashboard;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentDashboardBinding;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import android.location.Address;
+import android.location.Geocoder;
+
+import java.io.IOException;
+import java.util.List;
 
 public class DashboardFragment extends Fragment {
 
     private FragmentDashboardBinding binding;
+    private TextView locationText;
+    private LocationRequest locationRequest;
+    double latitude;
+    double longitude;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -26,7 +60,159 @@ public class DashboardFragment extends Fragment {
 
         final TextView textView = binding.textDashboard;
         dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        locationText = root.findViewById(R.id.locationText);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(2000);
+
+        ImageButton getLocationButton = root.findViewById(R.id.imageButton);
+        getLocationButton.setOnClickListener(this::getLocation);
+
+        Button next=root.findViewById(R.id.next_map);
+        next.setOnClickListener(this::next);
+
         return root;
+    }
+
+    public void getLocation(View view){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            if(ActivityCompat.checkSelfPermission(requireActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                if(isGPSEnabled()){
+                    LocationServices.getFusedLocationProviderClient(requireActivity())
+                            .requestLocationUpdates(locationRequest, new LocationCallback() {
+                                @Override
+                                public void onLocationResult(@NonNull LocationResult locationResult) {
+                                    super.onLocationResult(locationResult);
+                                    LocationServices.getFusedLocationProviderClient(requireActivity())
+                                            .removeLocationUpdates(this);
+
+                                    if(locationResult!=null && locationResult.getLocations().size()>0){
+                                        int index=locationResult.getLocations().size()-1;
+                                        double latitude= locationResult.getLocations().get(index).getLatitude();
+                                        double longitude=locationResult.getLocations().get(index).getLongitude();
+
+//                                        locationText.setText(latitude+", "+longitude);
+                                        getAddressFromLocation(latitude, longitude);
+
+
+                                    }
+                                }
+                            }, Looper.getMainLooper());
+                }
+                else{
+                    turnOnGPS();
+                }
+            }
+            else{
+                ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        }
+    }
+    public void next(View view){
+        String a=locationText.getText().toString();
+        String[] split = a.split(",");
+        if(split.length==2){
+
+        }
+        else{
+            LatLng location = getLocationFromAddress(requireContext(), a);
+            longitude=location.longitude;
+            latitude=location.latitude;
+            Log.d("STATE",latitude+", "+longitude);
+        }
+    }
+    public static LatLng getLocationFromAddress(Context context, String strAddress) {
+        Geocoder coder = new Geocoder(context);
+        List<Address> address;
+        LatLng latLng = null;
+
+        try {
+            address = coder.getFromLocationName(strAddress, 5);
+            if (address == null || address.isEmpty()) {
+                return null;
+            }
+
+            Address location = address.get(0);
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            latLng = new LatLng(latitude, longitude);
+
+            return latLng;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    private void getAddressFromLocation(double latitude, double longitude) {
+        Geocoder geocoder = new Geocoder(requireContext());
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+            if (addresses != null && addresses.size() > 0) {
+                Address address = addresses.get(0);
+                String addressString = address.getAddressLine(0); // Get the full address
+                locationText.setText(addressString);
+            } else {
+                locationText.setText("Address not found");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            locationText.setText("Error fetching address");
+        }
+    }
+
+    private void turnOnGPS(){
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+        builder.setAlwaysShow(true);
+
+        Task<LocationSettingsResponse> result = LocationServices.getSettingsClient(requireContext())
+                .checkLocationSettings(builder.build());
+
+        result.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+
+                try {
+                    LocationSettingsResponse response = task.getResult(ApiException.class);
+                    Toast.makeText(requireActivity(), "GPS is already turned on", Toast.LENGTH_SHORT).show();
+
+
+                } catch (ApiException e) {
+
+                    switch (e.getStatusCode()) {
+                        case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+
+                            try {
+                                ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                                resolvableApiException.startResolutionForResult(requireActivity(), 2);
+                            } catch (IntentSender.SendIntentException ex) {
+                                ex.printStackTrace();
+                            }
+                            break;
+
+                        case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                            //Device does not have location
+                            break;
+                    }
+                }
+            }
+        });
+    }
+    private boolean isGPSEnabled(){
+        LocationManager locationManager=null;
+        boolean isEnabled=false;
+
+        if(locationManager==null){
+            locationManager=(LocationManager)requireContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+        isEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        return isEnabled;
     }
 
     @Override
