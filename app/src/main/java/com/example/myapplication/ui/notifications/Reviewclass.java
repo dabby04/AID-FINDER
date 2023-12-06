bbca492c627bb0c1014c22eb6d4c4aed85d8f4d0
PainterW0lf <vanshikasingla2002@gmail.com>
2023-12-05 20:27:36 -0800

    rating bar works

--- app/src/main/java/com/example/myapplication/ui/notifications/Reviewclass.java
+++ app/src/main/java/com/example/myapplication/ui/notifications/Reviewclass.java
@@ -7,16 +7,34 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
 
 public class Reviewclass extends AppCompatActivity{
+
+
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rewiew_page1);
         Log.d("Reviewclass", "Reviewclass activity started");
+
+        RatingBar ratingBar = findViewById(R.id.ratingBar);
+
+//        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+//            @Override
+//            public void onRatingChanged(RatingBar ratingBar) {
+//
+//                Toast.makeText(getApplicationContext(), "Rated: " + rating, Toast.LENGTH_SHORT).show();
+//
+//
+//            }
+//        });
 
     }
 
